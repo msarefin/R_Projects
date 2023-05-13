bce8e9f5d197f38d7396c770bf556f03fb39dbfe
arefin <mohammed.arefin@gmail.com>
2023-05-13 01:31:00 +0000

    matrix learned

--- matrices.R
+++ matrices.R
@@ -12,3 +12,24 @@ colnames = c("col1","col2","col3")
 P <- matrix(c(3:14),nrow=4, byrow=TRUE, dimnames = list(rownames,colnames))
 
 print(P)
+
+#Accessing elements of a matrix
+
+print(P[1,3])
+print(P[4,2])
+print(P[2,])
+print(P[,3])
+
+
+#Matrix calculations
+
+matrix1 <- matrix(c(3,9,-1,4,2,6), nrow =2)
+matrix2 <- matrix(c(5,2,0,9,3,4), nrow =2)
+
+print(matrix1)
+print(matrix2)
+
+print(matrix1+matrix2)
+print(matrix1-matrix2)
+print(matrix1*matrix2)
+print(matrix1/matrix2)
