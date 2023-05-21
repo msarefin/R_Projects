516da49cae98b7db67d688858dfda76dd3c792c0
Sunny <mohammed.arefin@gmail.com>
2023-05-21 13:35:11 -0400

    learning how to access csv files in R

--- csvFiles.R
+++ csvFiles.R
@@ -1,1 +1,12 @@
 #Reading csv files
+
+csv.data <- read.csv("https://people.sc.fsu.edu/~jburkardt/data/csv/crash_catalonia.csv")
+
+print(csv.data)
+
+print(is.data.frame(csv.data))
+print(ncol(csv.data))
+print(nrow(csv.data))
+
+max.crash <- max(csv.data$Number.of.Crashes)
+print(max.crash)
