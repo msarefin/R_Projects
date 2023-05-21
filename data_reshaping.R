2a2ed86b550fbd5be4c9ab5db420b86cbbbb0f6f
msarefin <mohammed.arefin@gmail.com>
2023-05-21 16:21:58 -0400

    learning to work on CSV files in R

--- data_reshaping.R
+++ data_reshaping.R
@@ -1,46 +1,14 @@
 #Data Reshaping
 
-<<<<<<< HEAD
-#create vector objects
-=======
 ########### Joining Columns and Data frames ###########
 
 print("########### Joining Columns and Data frames ###########")
 
 # Create vector objects.
->>>>>>> 904725307a8eded308cd2ed6968a88b9f1b78700
 city <- c("Tampa","Seattle","Hartford","Denver")
 state <- c("FL","WA","CT","CO")
 zipcode <- c(33602,98104,06161,80294)
 
-<<<<<<< HEAD
-#combine above vectors into data frame
-address <- cbind(city,state,zipcode)
-
-#print a header
-cat('#### The First sata frame\n')
-
-print(address)
-
-new.address <- data.frame(
-	city = c("Lowry","Charlotte"),
-	state = c("Co","FL"),
-	zipcode = c("80230","33949")
-	stringsAsFactors = FALSE
-)
-
-#Print header
-cat("### The Second Data frame\n")
-
-#print the data frame
-print(new.address)
-
-all.addresses <- rbind(address, new.address)
-
-cat("### The Combined frame\n")
-
-print(all.addresses)
-=======
 # Combine above three vectors into one data frame.
 addresses <- cbind(city,state,zipcode)
 
@@ -98,4 +66,3 @@ print(molten.ships)
 
 recasted.ship <- cast(molten.ships, type+year~variable, sum)
 print(recasted.ship)
->>>>>>> 904725307a8eded308cd2ed6968a88b9f1b78700
