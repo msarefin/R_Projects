71f712a91e5372bf28a99f07a2e3a6efc572d9c8
msarefin <mohammed.arefin@gmail.com>
2023-05-24 00:10:37 -0400

    working with csv - next writing to csv

--- csvFiles.R
+++ csvFiles.R
@@ -1,14 +1,27 @@
 #Reading csv files
-
-csv.data <- read.csv("https://people.sc.fsu.edu/~jburkardt/data/csv/crash_catalonia.csv")
+print("********* Reading a CSV file *********")
+csv.data <- read.csv("input.csv")
 
 print(csv.data)
 
 print(is.data.frame(csv.data))
 print(ncol(csv.data))
 print(nrow(csv.data))
+print("********* max salary *********")
+sal <- max(csv.data$salary)
+print(sal)
+print("********* Query data with max salary *********")
+retval <- subset(csv.data, salary == max(csv.data$salary))
+print(retval)
+print("********* Query dept IT *********")
+retval <- subset(csv.data, dept =="IT")
+print(retval)
+print("********* Query salary >600 and dept IT *********")
 
-max.crash <- max(csv.data$Number.of.Crashes)
-print(max.crash)
+retval <- subset(csv.data, salary >600 & dept =="IT")
+print(retval)
 
-retval <- subset(csv.data, crashes == max(csv.data$Number.of.Crashes))
+print("********* Started on and after 2014 *********")
+
+retval <- subset(data, as.Date(start_date) > as.Date("2014-01-01"))
+print(retval)
