a2fd3f99ec96e53374138af5055be69de96c774a
msarefin <mohammed.arefin@gmail.com>
2023-06-19 15:55:03 -0400

    Learning to write bin fie

--- Binary_file_read_write.R
+++ Binary_file_read_write.R
@@ -1,15 +1,24 @@
+if(file.exists("mtcars.csv")){
+  file.remove("mtcars.csv")
+  print("mtcars.csv was found and removed")
+}else{
+  print("mtcars.csv not found")
+}
+
+
 write.table(mtcars, file = "mtcars.csv",row.names = FALSE, na ="", col.names = TRUE, sep=",")
 
 #Read the CSV file
 new.mtcars <- read.table("mtcars.csv",sep=",", header = TRUE, nrow = 5)
 
-#write the .dat file
+
+# Create a connection object to write the binary file using mode "wb".
 write.filename = file("./bitmtcars.dat","wb")
 
-#create connection to write the file 
+# Write the column names of the data frame to the connection object.
 writeBin(colnames(new.mtcars),write.filename)
 
-#write into the file
+# Write the records in each of the column to the file.
 writeBin(c(new.mtcars$cyl, new.mtcars$am, new.mtcars$gear),write.filename)
 
 close(write.filename)
