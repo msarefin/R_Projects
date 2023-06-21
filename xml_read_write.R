dcdf58d65d3968893d8c42d252b571ba9296fcfe
msarefin <mohammed.arefin@gmail.com>
2023-06-20 23:31:16 -0400

    learning xml parse

---
+++ xml_read_write.R
@@ -0,0 +1,17 @@
+library("XML")
+library("methods")
+result <- xmlParse(file = "input.xml")
+print(result)
+
+rootnode <- xmlRoot(result)
+rootsize <- xmlSize(rootnode)
+print(rootsize)
+
+print(rootnode[1])
+
+print(rootnode[[1]][[5]])
+
+print(rootnode[[3]][[2]])
+
+xmlDataFrame <- xmlToDataFrame("./input.xml")
+print(xmlDataFrame)
