9c0d8251044be3ba47e2beaa51983ef874e4bffd
msarefin <mohammed.arefin@gmail.com>
2023-06-26 20:40:19 -0400

    learning xml in R

--- webdata_read.R
+++ webdata_read.R
@@ -3,3 +3,22 @@
 #install.packages("stringr")
 #install.packages("plyr")
 
+library("XML")
+library("methods")
+result <- xmlParse(file="input.xml")
+print(result)
+
+rootnode <- xmlRoot(result)
+
+rootsize <- xmlSize(rootnode)
+
+print(rootsize)
+
+print(rootnode[1])
+
+print(rootnode[[1]][[1]])
+print(rootnode[[1]][[5]])
+print(rootnode[[1]][[2]])
+
+xmldataframe <- xmlToDataFrame("input.xml")
+print(xmldataframe)
