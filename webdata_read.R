e69558327e8f8f5b574912e4d18e7c8cd2ed6d42
msarefin <mohammed.arefin@gmail.com>
2023-07-25 08:46:00 -0400

    still working

--- webdata_read.R
+++ webdata_read.R
@@ -1,24 +1,9 @@
-#install.packages("RCurl")
-#install.packages("XML")
-#install.packages("stringr")
-#install.packages("plyr")
+install.packages("RCurl")
+install.packages("XML")
+install.packages("stringr")
+install.packages("plyr")
 
-library("XML")
-library("methods")
-result <- xmlParse(file="input.xml")
-print(result)
+#get Data from the web
 
-rootnode <- xmlRoot(result)
-
-rootsize <- xmlSize(rootnode)
-
-print(rootsize)
-
-print(rootnode[1])
-
-print(rootnode[[1]][[1]])
-print(rootnode[[1]][[5]])
-print(rootnode[[1]][[2]])
-
-xmldataframe <- xmlToDataFrame("input.xml")
-print(xmldataframe)
+url <- "http://www.geos.ed.ac.uk/~weather/jcmb_ws/"
+links <- getHTMLLinks(url);
