eb1b5e45609d84dd4cbe8343b4dc95fc743768a1
msarefin <mohammed.arefin@gmail.com>
2023-07-26 23:00:26 -0400

    failed to learn reading from web

--- webdata_read.R
+++ webdata_read.R
@@ -6,4 +6,13 @@ install.packages("plyr")
 #get Data from the web
 
 url <- "http://www.geos.ed.ac.uk/~weather/jcmb_ws/"
-links <- getHTMLLinks(url);
+links <- getHTMLLinks(url)
+filenames <- links[str_detect(links, "JCMB_2015")]
+filenames_list <- as.list(filenames)
+
+downloadcsv <- function (mainurl,filename) {
+  filedetails <- str_c(mainurl,filename)
+  download.file(filedetails,filename)
+}
+
+l_ply(filenames,downloadcsv,mainurl = "http://www.geos.ed.ac.uk/~weather/jcmb_ws/")
