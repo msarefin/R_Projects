ca76b969419e52780545e224c9ea5536052996e3
msarefin <mohammed.arefin@gmail.com>
2023-07-26 23:52:52 -0400

    failed to learn reading from web

--- webdata_read.R
+++ webdata_read.R
@@ -1,18 +1,10 @@
-install.packages("RCurl")
-install.packages("XML")
-install.packages("stringr")
-install.packages("plyr")
+install.packages("tidyverse")
+install.packages("rvest")
+library(tidyverse)
+library(rvest)
 
-#get Data from the web
+url = "https://en.wikipedia.org/wiki/World_Happiness_Report"
 
-url <- "http://www.geos.ed.ac.uk/~weather/jcmb_ws/"
-links <- getHTMLLinks(url)
-filenames <- links[str_detect(links, "JCMB_2015")]
-filenames_list <- as.list(filenames)
-
-downloadcsv <- function (mainurl,filename) {
-  filedetails <- str_c(mainurl,filename)
-  download.file(filedetails,filename)
-}
-
-l_ply(filenames,downloadcsv,mainurl = "http://www.geos.ed.ac.uk/~weather/jcmb_ws/")
+happy = read_html(url)%>%
+  html_element('tables.wikitable')%>%
+  html_table()
