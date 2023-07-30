8c795669ff9fce37eed242e644e9ec1fa0d901ea
msarefin <mohammed.arefin@gmail.com>
2023-07-29 23:09:53 -0400

    learning about RCurl

--- webdata_read.R
+++ webdata_read.R
@@ -1,14 +1,30 @@
-install.packages("RCurl")
-install.packages("XML")
-install.packages("stringr")
-install.packages("plyr")
 install.packages("rvest")
-
-library(RCurl)
-library(XML)
-library(stringr)
-library(plyr)
 library(rvest)
+link <- "https://www.nytimes.com/"
+NYT_page <- read_html(link)
+NYT_page; 
 
-url <- "http://www.geos.ed.ac.uk/~weather/jcmb_ws/"
-link <- getHTMLLinks(url)
+#NYT_page %>% 
+ # html_elements(xpath = "")
+
+#NYT_page %>%
+ # html_elements(css = "")
+
+summaries_css <- NYT_page %>%
+  html_elements(css = ".summary-class")
+
+head(summaries_css)
+
+summaries_xpath <- NYT_page %>%
+  html_elements(xpath = "//*[contains(@class, 'summary-class')]")
+
+head(summaries_xpath)
+
+
+
+####################
+
+install.packages("RCurl")
+library(RCurl)
+
+sample
