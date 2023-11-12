ff99b740d4cc93c81efb2ba62171938d4d991c6a
msarefin <mohammed.arefin@gmail.com>
2023-11-12 10:39:32 -0500

    Learning the R again

---
+++ LearningR.R
@@ -0,0 +1,19 @@
+#Package for 3D Charts
+install.packages("plotrix")
+
+mystring <- "Hello World!"
+print(mystring)
+cat(mystring)
+
+#This is just comment
+
+if(FALSE){
+  "This is a way to create a multi level comment
+  since the parameter is always FALSE there is no out put!!!"
+}
+
+#Data types in R
+
+
+# This is just a multi line comment 
+# all i have to put in s a hash tag using 
