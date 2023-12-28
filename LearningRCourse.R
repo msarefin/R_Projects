055057847c8e859d1d72be543f33c77cd4416a9e
arefin <mohammed.arefin@gmail.com>
2023-12-28 14:59:38 +0000

    Leraning R from Google analytics

--- LearningRCourse.R
+++ LearningRCourse.R
@@ -1,2 +1,21 @@
 #Learning R from Google Analytics
 
+x <- c(1,3,5)
+names(x) <- c("a","b","c")
+
+list("a", 1L, 1.5, TRUE)
+
+z <- list(list(list(1 , 3, 5)))
+
+str(list("a", 1L, 1.5, TRUE))
+
+
+#nameing list
+list('Chicago' = 1, 'New York' = 2, 'Los Angeles' = 3)
+
+install.packages("tidyverse")
+library(tidyverse)
+library(lubridate)
+
+today()
+now()
