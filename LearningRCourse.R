9df285732dcd723fbc7b2822649ca733def7b842
Sunny <mohammed.arefin@gmail.com>
2024-01-07 17:44:54 -0500

    update

--- LearningRCourse.R
+++ LearningRCourse.R
@@ -116,7 +116,11 @@ diamonds %>% group_by(cut) %>% summarize(n())
 #get the distict number of cut 
 diamonds %>% summarize(n_distinct(cut))
 
+#read_csv(): comma-separated values (.csv) files
+#read_tsv(): tab-separated values files
+#read_delim(): general delimited files
+#read_fwf(): fixed-width files
+#read_table(): tabular files where columns are separated by white-space
+#read_log(): web log files
 
-
-
-
+readr_example()
