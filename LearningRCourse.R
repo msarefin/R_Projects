55b13060d569a3533b008d15aa521dafef1fb5b4
arefin <mohammed.arefin@gmail.com>
2024-02-03 17:12:12 +0000

    learning pip function in R

--- LearningRCourse.R
+++ LearningRCourse.R
@@ -209,4 +209,7 @@ penguins2 <- penguins %>% arrange(-bill_length_mm)
 head(penguins2)
 
 
-penguins %>% group_by(island) %>% drop_na() %>%  summarize(mean_bill_length_mm = mean(bill_length_mm))
+penguins %>% 
+  group_by(island) %>% 
+  drop_na() %>%  
+  summarize(mean_bill_length_mm = mean(bill_length_mm))
