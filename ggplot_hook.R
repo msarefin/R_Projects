0b95c81c1deaade9f09ac4878127c5dda3b5429f
arefin <mohammed.arefin@gmail.com>
2024-05-02 03:18:10 +0000

    saving work done on Rmd files

--- ggplot_hook.R
+++ ggplot_hook.R
@@ -1,7 +1,4 @@
-penguins %>%
-  drop_na(sex) %>%
-  ggplot(aes(x=flipper_length_mm,y=body_mass_g)) +
-  geom_point(aes(color=species),
-             shape=species) +
-  facet_wrap(~sex)
-
+penguins %>% 
+  drop_na(sex) %>% 
+  ggplot(aes(x= flipper_length_mm, y = body_mass_g))+
+  geom_point(aes(color = species, shape = species))+ facet_wrap(~sex)
