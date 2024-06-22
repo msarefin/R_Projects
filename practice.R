f0f612a145c0e0c73d50f139cea794a729030be0
arefin <mohammed.arefin@gmail.com>
2024-06-22 22:12:00 +0000

    learning R-object types

--- practice.R
+++ practice.R
@@ -14,3 +14,71 @@ if(FALSE){
   with falls passed on to it"
 }
 
+#The variable types in R are determined the data type of the R-Object
+# R-Object types
+# Vectors
+# Lists
+# Matrices
+# Arrays
+# Factors
+# Data Frames
+
+################### Vectors ################### 
+
+#There are 6 classes of vectors 
+# Logical
+# Neumeric
+# Integer
+# Complex
+# Character
+# Raw
+
+
+################### Logical Vector ################### 
+
+v1 <- TRUE
+v2 <- FALSE
+
+print(class(v1))
+
+################### Numeric Vector ################### 
+
+v3 <- 23.5
+
+print(class(v3))
+
+################### Integer Vector ################### 
+
+v4 = 2L
+
+print(class(v4))
+
+################### Complex Vector ################### 
+
+v5 <- 2+5i
+print(class(v5))
+
+################### Character Vector ################### 
+
+v6 <- "TRUE"
+print(class(v6))
+
+################### Raw Vector ################### 
+
+v7 <- charToRaw(v6)
+print(v7)
+
+v8 <- charToRaw("Hello")
+print(v8)
+print(class(v7))
+print(class(v8))
+
+################### Multiple element Vector ################### 
+
+apple <- c("Red","Blue","Green")
+print(class(apple))
+
+v <- c(1,2,3,4,5+2i)
+print(class(v))
+
+
