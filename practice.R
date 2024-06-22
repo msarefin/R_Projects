eda4101c2430bfcea0c75fc09318f321cbb2c2fd
arefin <mohammed.arefin@gmail.com>
2024-06-22 21:08:16 +0000

    created practic.R file - practicing R programming

---
+++ practice.R
@@ -0,0 +1,16 @@
+#Book link - https://r4ds.hadley.nz/
+
+install.packages("tidyverse")
+library(tidyverse)
+
+#R variables
+mystring <- "Hello R!"
+print(mystring)
+
+#R only supports single line comments
+
+if(FALSE){
+  "One way to have multi line comments is to create if statement 
+  with falls passed on to it"
+}
+
