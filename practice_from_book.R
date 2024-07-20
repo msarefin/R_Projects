89da0e80e3e2578080f59c26bdcbb68868f41d66
arefin <mohammed.arefin@gmail.com>
2024-07-20 21:26:00 +0000

    Learning from book ggplot

--- practice_from_book.R
+++ practice_from_book.R
@@ -9,7 +9,8 @@ library(ggthemes)
 palmerpenguins::penguins
 
 glimpse(penguins)
-View(penguins)
+View(penguins);
 
 
-ggplot(data = penguins)
+ggplot(data = penguins, mapping=aes(x=flipper_length_mm, y = body_mass_g))+geom_point(); 
+
