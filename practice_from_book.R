ec5982a0f771863291e118490b48989bb4989cdd
arefin <mohammed.arefin@gmail.com>
2024-07-27 23:36:44 +0000

    ggplot(data, mapping)+geom_plot()+geom_smooth()

--- practice_from_book.R
+++ practice_from_book.R
@@ -12,7 +12,12 @@ glimpse(penguins)
 View(penguins);
 
 
-ggplot(data = penguins, 
-       mapping=aes(x=flipper_length_mm, y = body_mass_g))+
-  geom_point() 
+ggplot(data = penguins, mapping=aes(x=flipper_length_mm, y = body_mass_g))+ geom_point() 
 
+ggplot(data = penguins)+geom_point(mapping = aes(x= flipper_length_mm, y = body_mass_g))
+ggplot(penguins, mapping = aes(flipper_length_mm,body_mass_g))+geom_point()
+ggplot(penguins)+geom_point(aes(flipper_length_mm, body_mass_g))
+
+ggplot(data = penguins, mapping = aes(x= flipper_length_mm, y = body_mass_g, color = species))+geom_point()+geom_smooth(method = "lm")
+
+       
