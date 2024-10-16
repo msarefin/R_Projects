fa5ca82fbe61ec07e661e9811b2ed20c962d1dbc
arefin <mohammed.arefin@gmail.com>
2024-10-16 03:49:48 +0000

    select function

--- practice_from_book.R
+++ practice_from_book.R
@@ -195,4 +195,24 @@ flights |> mutate(gain = dep_delay-arr_delay, speed = distance/air_time, .after 
 
 #mutate with .keep argument 
 
+# Control which columns from .data are retained in the output. Grouping columns and columns created by ... are always kept.
+# "all" retains all columns from .data. This is the default.
+# "used" retains only the columns used in ... to create new columns. This is useful for checking your work, as it displays inputs and outputs side-by-side.
+# "unused" retains only the columns not used in ... to create new columns. This is useful if you generate new columns, but no longer need the columns used to generate them.
+# "none" doesn't retain any extra columns from .data. Only the grouping variables and columns created by ... are kept.
+
 flights |> mutate(gain = dep_delay - arr_delay, speed = distance/air_time * 60, .keep = "used")
+
+#select 
+
+flights |> select(year, month, day)
+
+flights |> select(year:day)
+
+flights |> select(!year:day)
+
+flights |> select(where(is.character))
+
+
+
+
