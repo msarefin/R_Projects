e633f013895743fd78c29bd6fa3f3e3e485bd18c
msarefin <mohammed.arefin@gmail.com>
2025-05-27 00:15:48 -0400

    Chapter 15

--- practice_from_book.R
+++ practice_from_book.R
@@ -2754,3 +2754,45 @@ str_to_upper(c("i","i"),locale = "tr")
 str_sort(c("a","c","ch","h","z"))
 str_sort(c("a","c","ch","h","z"), locale = "cs")
 
+# https://r4ds.hadley.nz/regexps.html#introduction
+
+
+# Pattern Basics - https://r4ds.hadley.nz/regexps.html#introduction
+
+# Most punctuation characters, like ., +, *, [, ], and ?, 
+# have special meanings2 and are called metacharacters. 
+
+str_view(fruit, "berry")
+str_view(c("a", "ab", "ae", "bd", "ea", "eab"), "a.")
+str_view(fruit, "a...e")
+
+# ? makes a pattern optional (i.e. it matches 0 or 1 times)
+# + lets a pattern repeat (i.e. it matches at least once)
+# * lets a pattern be optional or repeat (i.e. it matches any number of times, including 0).
+
+# ab? matches an "a", optionally followed by a "b".
+str_view(c("a", "ab", "abb"), "ab?")
+
+# ab+ matches an "a", followed by at least one "b".
+str_view(c("a", "ab", "abb"), "ab+")
+
+# ab* matches an "a", followed by any number of "b"s.
+str_view(c("a", "ab", "abb"), "ab*")
+
+# Character classes are defined by [] and let you match a set of characters, 
+# e.g., [abcd] matches “a”, “b”, “c”, or “d”. You can also invert the match 
+# by starting with ^: [^abcd] matches anything except “a”, “b”, “c”, or “d”. 
+
+str_view(words, "[aeiou]x[aeiou]")
+str_view(words, "[^aeiou]y[^aeiou]")
+
+
+# You can use alternation, |, to pick between one or more alternative patterns.
+
+str_view(fruit, "apple|melon|nut")
+str_view(fruit, "aa|ee|ii|oo|uu")
+
+
+
+# 15.3 Key functions - https://r4ds.hadley.nz/regexps.html#sec-stringr-regex-funs
+
