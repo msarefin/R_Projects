3a22f796494119c9fecb3f6e9a74dc2274673db9
arefin <mohammed.arefin@gmail.com>
2025-11-16 03:47:10 +0000

    update

--- Learning R Form Book/R_for_Data_Science/Chapter 1.R
+++ Learning R Form Book/R_for_Data_Science/Chapter 1.R
@@ -69,4 +69,51 @@ penguins |> ggplot(mapping = aes(x = flipper_length_mm, y = body_mass_g))+
     x = "Flipper Length (mm)",y = "Body Mass (g)",
     color = "Species", shape = "Species"
   )
-  
+
+
+penguins |>
+  ggplot(mapping = aes(x = flipper_length_mm, y = body_mass_g))+
+  geom_point(mapping= aes(color = bill_depth_mm))+
+  geom_smooth() 
+
+penguins |>
+  ggplot(mapping = aes(x = flipper_length_mm, y = body_mass_g))+
+  geom_point(mapping= aes(color = bill_depth_mm))+
+  geom_smooth(se = FALSE)
+
+
+penguins |>
+  ggplot(mapping = aes(x = flipper_length_mm , y = body_mass_g))+
+  geom_point()+
+  geom_smooth()
+
+ggplot()+
+  geom_point(data = penguins, mapping = aes(x = flipper_length_mm, y = body_mass_g))+
+  geom_smooth(data = penguins, mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+# A categorical variable - https://r4ds.hadley.nz/data-visualize.html#a-categorical-variable
+
+penguins |> 
+  ggplot(mapping = aes(x = species))+
+  geom_bar()
+
+
+penguins|> 
+  ggplot(mapping = aes(x = species, fill = island))+
+  geom_bar()
+
+
+penguins |> 
+  ggplot(mapping = aes(x = fct_infreq(island)))+
+  geom_bar()
+
+
+# A numerical variable - https://r4ds.hadley.nz/data-visualize.html#a-numerical-variable
+
+penguins |>
+  ggplot(mapping = aes(x = body_mass_g))+
+  geom_histogram(binwidth = 200) # binwidth controls the width of the bars
+
+penguins |>
+  ggplot(mapping = aes(x = body_mass_g)) +
+  geom_histogram(binwidth = 20)
